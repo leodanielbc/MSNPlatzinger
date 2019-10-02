4ff7c4f28806236fbb8f5b8ac329b375492c37d7
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-10-02 13:18:53 -0500

    Uso de interfaces de TypeScript y su implementacion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    let myUser:User = {
+      nick: 'Leonardo',
+      subnick: 'Hola!',
+      //age: 28,
+      email: 'leo@gmail.com',
+      friend: true,
+      uid: 1
+    }
+    let users:User[]=[
+      myUser
+    ];
+    console.log(myUser);
+  }
 
   ngOnInit() {
   }
---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,8 @@
+export interface User{
+    nick: string;
+    subnick?: string;
+    age?: number;//con el simbolo ? le decimos que este atributo es opcional
+    email: string;
+    friend: boolean;
+    uid:any;
+}
