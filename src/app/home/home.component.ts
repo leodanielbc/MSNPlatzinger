8b40c77de6ae7de384cf79af1b12a0e0e8b05dea
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-10-02 13:34:46 -0500

    directiva NgFor aplicado a la lista de usuarios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,19 +8,34 @@ import { User } from '../interfaces/user';
 })
 export class HomeComponent implements OnInit {
 
+  friends: User[];
   constructor() {
-    let myUser:User = {
+    let usuario1:User = {
       nick: 'Leonardo',
       subnick: 'Hola!',
-      //age: 28,
+      age: 28,
       email: 'leo@gmail.com',
       friend: true,
       uid: 1
-    }
-    let users:User[]=[
-      myUser
-    ];
-    console.log(myUser);
+    };
+    let usuario2:User={
+      nick: 'Yuliana',
+      subnick: 'Mi mensaje personal',
+      age: 25,
+      email: 'yuliana@platzi.com',
+      friend: true,
+      uid:2
+    };
+    let usuario3:User={
+      nick: 'Maria',
+      subnick: 'Mi mensaje personal',
+      age: 22,
+      email: 'maria@platzi.com',
+      friend: true,
+      uid:3
+    };
+
+    this.friends=[usuario1, usuario2, usuario3];
   }
 
   ngOnInit() {
