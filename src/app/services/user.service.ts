b63ff88b3437680b27a08b3729e06e247453dfc3
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-10-10 02:45:33 -0500

    Aceptando las solicitudes de amistad(modal)

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -27,6 +27,6 @@ export class UserService {
   }
   addFriend(userId, friendId){
     this.angularFireDatabase.object('users/'+userId+'/friends/'+friendId).set(friendId);
-    return this.angularFireDatabase.object('users/'+friendId+'/friends'+userId).set(userId);
+    return this.angularFireDatabase.object('users/'+friendId+'/friends/'+userId).set(userId);
   }
 }
